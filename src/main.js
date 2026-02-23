c374dbf7b058d0209c66522df52675237d56423a
tyakura <attayaarkarna12@gmail.com>
2026-02-23 16:31:34 +0700

    second commit

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import Swiper from 'swiper'
+import { Navigation, Autoplay } from 'swiper/modules'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+new Swiper('.swiper', {
+    modules: [Navigation,Autoplay],
+    slidesPerView: 2,
+    spaceBetween: 24,
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+    breakpoints: {
+        1024: {slidesPerView: 1},
+        640: {slidesPerView: 1}
+    },
 
-setupCounter(document.querySelector('#counter'))
+});
